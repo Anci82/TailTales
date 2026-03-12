f5cf7b7658757f9ed0dc3b6a89fa0c47af36834c
Anci <anashuttleworth@hotmail.com>
2026-03-12 15:47:23 +0000

    Care reminders - How to use page

--- TailTales/common/urls.py
+++ TailTales/common/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from TailTales.common.views import HomeView, AboutView, DashboardView
+from TailTales.common.views import HomeView, AboutView, DashboardView, HowItWorksView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('about/', AboutView.as_view(), name='about'),
+    path('how-it-works/', HowItWorksView.as_view(), name='how-it-works'),
     path('dashboard/', DashboardView.as_view(), name='dashboard'),
 ]
--- TailTales/common/views.py
+++ TailTales/common/views.py
@@ -3,7 +3,8 @@ from django.views import generic as views
 from datetime import timedelta
 from django.utils import timezone
 
-from TailTales.care.models import Appointment
+from TailTales.care.models import Appointment, PreventiveCare
+
 
 def get_tomorrows_appointments(user):
     tomorrow = timezone.localdate() + timedelta(days=1)
@@ -18,6 +19,18 @@ def get_tomorrows_appointments(user):
         .select_related('pet', 'service_contact')
         .order_by('pet__name', 'title')
     )
+def get_tomorrows_preventive_care(user):
+    tomorrow = timezone.localdate() + timedelta(days=1)
+
+    return (
+        PreventiveCare.objects
+        .filter(
+            pet__owner=user,
+            next_due_date=tomorrow,
+        )
+        .select_related('pet')
+        .order_by('pet__name', 'care_type')
+    )
 
 class HomeView(views.TemplateView):
     template_name = 'common/home.html'
@@ -26,6 +39,9 @@ class HomeView(views.TemplateView):
 class AboutView(views.TemplateView):
     template_name = 'common/about.html'
 
+class HowItWorksView(views.TemplateView):
+    template_name = 'common/how-it-works.html'
+
 
 class DashboardView(LoginRequiredMixin, views.TemplateView):
     template_name = 'common/dashboard.html'
@@ -33,5 +49,6 @@ class DashboardView(LoginRequiredMixin, views.TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['tomorrow_appointments'] = get_tomorrows_appointments(self.request.user)
+        context['tomorrow_preventive_care'] = get_tomorrows_preventive_care(self.request.user)
         return context
 
