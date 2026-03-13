98e8a870f21b0c42a42cfa4fd7cf194073b17d13
Anci <anashuttleworth@hotmail.com>
2026-03-13 11:07:35 +0000

    Password reset

--- TailTales/accounts/urls.py
+++ TailTales/accounts/urls.py
@@ -5,10 +5,48 @@ from TailTales.accounts.views import RegisterView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
+
     path(
         'login/',
         auth_views.LoginView.as_view(template_name='accounts/login.html'),
         name='login',
     ),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path(
+        'logout/',
+        auth_views.LogoutView.as_view(),
+        name='logout',
+    ),
+
+    path(
+        'password-reset/',
+        auth_views.PasswordResetView.as_view(
+            template_name='accounts/password_reset.html',
+            email_template_name='accounts/password_reset_email.txt',
+            subject_template_name='accounts/password_reset_subject.txt',
+            success_url='/accounts/password-reset/done/',
+        ),
+        name='password_reset',
+    ),
+    path(
+        'password-reset/done/',
+        auth_views.PasswordResetDoneView.as_view(
+            template_name='accounts/password_reset_done.html',
+        ),
+        name='password_reset_done',
+    ),
+    path(
+        'reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name='accounts/password_reset_confirm.html',
+            success_url='/accounts/reset/done/',
+        ),
+        name='password_reset_confirm',
+    ),
+    path(
+        'reset/done/',
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name='accounts/password_reset_complete.html',
+        ),
+        name='password_reset_complete',
+    ),
 ]
